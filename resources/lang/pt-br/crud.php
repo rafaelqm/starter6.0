64d05ec2cdd4b28e162f26e1539cd0df39b048c0
Rafael Querino Moreira <rafaelqm@gmail.com>
2020-01-11 00:43:01 -0300

    Localization pt-br added

---
+++ resources/lang/pt-br/crud.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'add_new'      => 'Adicionar novo',
+    'cancel'       => 'Cancelar',
+    'save'         => 'Salvar',
+    'edit'         => 'Editar',
+    'detail'       => 'Detalhes',
+    'back'         => 'Voltar',
+    'action'       => 'Ação',
+    'id'           => 'Id',
+    'created_at'   => 'Criado em',
+    'updated_at'   => 'Atualizado em',
+    'deleted_at'   => 'Removido em',
+    'are_you_sure' => 'Tem certeza?',
+];
