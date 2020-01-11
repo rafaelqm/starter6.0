65a33c9ec134a710b575bcfc2f30d7e079238cf1
Rafael Querino Moreira <rafaelqm@gmail.com>
2020-01-11 00:21:13 -0300

    Ajusts on configs

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -10,46 +10,26 @@ return [
     */
 
     'path' => [
-
-        'migration'         => database_path('migrations/'),
-
-        'model'             => app_path('Models/'),
-
-        'datatables'        => app_path('DataTables/'),
-
-        'repository'        => app_path('Repositories/'),
-
-        'routes'            => base_path('routes/web.php'),
-
-        'api_routes'        => base_path('routes/api.php'),
-
-        'request'           => app_path('Http/Requests/'),
-
-        'api_request'       => app_path('Http/Requests/API/'),
-
-        'controller'        => app_path('Http/Controllers/'),
-
-        'api_controller'    => app_path('Http/Controllers/API/'),
-
-        'repository_test'   => base_path('tests/Repositories/'),
-
-        'api_test'          => base_path('tests/APIs/'),
-
-        'tests'             => base_path('tests/'),
-
-        'views'             => resource_path('views/'),
-
-        'schema_files'      => resource_path('model_schemas/'),
-
-        'templates_dir'     => resource_path('infyom/infyom-generator-templates/'),
-
-        'seeder'            => database_path('seeds/'),
-
-        'database_seeder'   => database_path('seeds/DatabaseSeeder.php'),
-
-        'modelJs'           => resource_path('assets/js/models/'),
-
-        'factory'           => database_path('factories/'),
+        'migration' => database_path('migrations/'),
+        'model' => app_path('Models/'),
+        'datatables' => app_path('DataTables/'),
+        'repository' => app_path('Repositories/'),
+        'routes' => base_path('routes/web.php'),
+        'api_routes' => base_path('routes/api.php'),
+        'request' => app_path('Http/Requests/'),
+        'api_request' => app_path('Http/Requests/API/'),
+        'controller' => app_path('Http/Controllers/'),
+        'api_controller' => app_path('Http/Controllers/API/'),
+        'repository_test' => base_path('tests/Repositories/'),
+        'api_test' => base_path('tests/APIs/'),
+        'tests' => base_path('tests/'),
+        'views' => resource_path('views/'),
+        'schema_files' => resource_path('model_schemas/'),
+        'templates_dir' => resource_path('infyom/infyom-generator-templates/'),
+        'seeder' => database_path('seeds/'),
+        'database_seeder' => database_path('seeds/DatabaseSeeder.php'),
+        'modelJs' => resource_path('assets/js/models/'),
+        'factory' => database_path('factories/'),
     ],
 
     /*
@@ -60,26 +40,16 @@ return [
     */
 
     'namespace' => [
-
-        'model'             => 'App\Models',
-
-        'datatables'        => 'App\DataTables',
-
-        'repository'        => 'App\Repositories',
-
-        'controller'        => 'App\Http\Controllers',
-
-        'api_controller'    => 'App\Http\Controllers\API',
-
-        'request'           => 'App\Http\Requests',
-
-        'api_request'       => 'App\Http\Requests\API',
-
-        'repository_test'   => 'Tests\Repositories',
-
-        'api_test'          => 'Tests\APIs',
-
-        'tests'             => 'Tests',
+        'model' => 'App\Models',
+        'datatables' => 'App\DataTables',
+        'repository' => 'App\Repositories',
+        'controller' => 'App\Http\Controllers',
+        'api_controller' => 'App\Http\Controllers\API',
+        'request' => 'App\Http\Requests',
+        'api_request' => 'App\Http\Requests\API',
+        'repository_test' => 'Tests\Repositories',
+        'api_test' => 'Tests\APIs',
+        'tests' => 'Tests',
     ],
 
     /*
@@ -89,7 +59,7 @@ return [
     |
     */
 
-    'templates'         => 'coreui-templates',
+    'templates' => 'coreui-templates',
 
     /*
     |--------------------------------------------------------------------------
@@ -107,8 +77,7 @@ return [
     |
     */
 
-    'api_prefix'  => 'api',
-
+    'api_prefix' => 'api',
     'api_version' => 'v1',
 
     /*
@@ -119,15 +88,10 @@ return [
     */
 
     'options' => [
-
         'softDelete' => true,
-
         'save_schema_file' => true,
-
-        'tables_searchable_default' => false,
-
+        'tables_searchable_default' => true,
         'repository_pattern' => true,
-
         'excluded_fields' => ['id'], // Array of columns that doesn't required while creating module
     ],
 
@@ -139,14 +103,10 @@ return [
     */
 
     'prefixes' => [
-
         'route' => '',  // using admin will create route('admin.?.index') type routes
-
-        'path' => '',
-
-        'view' => '',  // using backend will create return view('backend.?.index') type the backend views directory
-
-        'public' => '',
+        //'path' => '',
+        //'view' => '',  // using backend will create return view('backend.?.index') type the backend views directory
+        //'public' => '',
     ],
 
     /*
@@ -157,18 +117,12 @@ return [
     */
 
     'add_on' => [
-
-        'swagger'       => false,
-
-        'tests'         => true,
-
-        'datatables'    => false,
-
-        'menu'          => [
-
-            'enabled'       => true,
-
-            'menu_file'     => 'layouts/menu.blade.php',
+        'swagger' => true,
+        'tests' => true,
+        'datatables' => true,
+        'menu' => [
+            'enabled' => true,
+            'menu_file' => 'layouts/menu.blade.php',
         ],
     ],
 
@@ -180,14 +134,10 @@ return [
     */
 
     'timestamps' => [
-
-        'enabled'       => true,
-
-        'created_at'    => 'created_at',
-
-        'updated_at'    => 'updated_at',
-
-        'deleted_at'    => 'deleted_at',
+        'enabled' => true,
+        'created_at' => 'created_at',
+        'updated_at' => 'updated_at',
+        'deleted_at' => 'deleted_at',
     ],
 
     /*
@@ -205,7 +155,6 @@ return [
     |
     */
     'from_table' => [
-
         'doctrine_mappings' => [],
     ],
 
